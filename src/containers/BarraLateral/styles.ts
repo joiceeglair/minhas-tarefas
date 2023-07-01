ca95593d210216d18de9fbf3a20db87189f10d38
Joice Eglair <116314841+joiceeglair@users.noreply.github.com>
2023-07-01 15:04:06 -0300

    Create styles.ts

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eeee;
+  height: 100vh; /* vh PARA OCULPAR TODO ESPAÇO ABAIXO */
+`
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+  margin-top: 16px;
+`
